f1087aad0bdd43e885071f9dc8787705d0635ab1
Antonio MG <tonio.mg@gmail.com>
2013-05-14 21:55:52 +0100

    Added notes view

--- gen/com/example/notepad/R.java
+++ gen/com/example/notepad/R.java
@@ -14,18 +14,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button1=0x7f070000;
+        public static final int editText1=0x7f070001;
+        public static final int editText2=0x7f070002;
+        public static final int listView1=0x7f070003;
+        public static final int menu_settings=0x7f070004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_add_note=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_add_note=0x7f060000;
+        public static final int activity_main=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_add_note=0x7f040003;
     }
     public static final class style {
         /** 
--- src/com/example/notepad/MainActivity.java
+++ src/com/example/notepad/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.notepad;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +17,25 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
+		
+		ArrayList<String> notesItems = new ArrayList<String>();
+		ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notesItems);
+		
+		//Get reference to the listview
+		ListView lv = (ListView)findViewById(R.id.listView1);
+		lv.setAdapter(aa);
+		
+		
+		
+		Button addButton = (Button)findViewById(R.id.button1);
+		addButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getBaseContext(), AddNoteActivity.class);
+				startActivity(intent);	
+			}
+		});
 	}
 
 }
